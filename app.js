8b894b2b5ee3bc94b980e819a489fb51a79c5cf5
JuanCarlosGardy <juancarlosgardy6@gmail.com>
2026-02-04 06:54:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -431,8 +431,8 @@ function printLast() {
     </head>
     <body>
       <div class="empresa">OBRANTIS, S.L.</div>
-      <div class="empresa-sub">Control interno de gastos</div>
-      <div class="h1">CONTROL DE GASTOS OBRANTIS</div>
+      <div class="empresa-sub">Control interno de gastos OBRANTIS</div>
+      <div class="h1">CONTROL DE GASTOS</div>
       <div class="sub">Ticket / ficha del último registro</div>
 
       <div class="hr"></div>
